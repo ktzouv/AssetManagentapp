38ae0173422c8df69b10dde8a3a8473d4d81035d
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-07-14 11:32:31 -0500

    Update Ping-IpRange.ps1
    Added workflow example

--- Ping-IpRange.ps1
+++ Ping-IpRange.ps1
@@ -43,83 +43,164 @@
   #>
 
 
-  [CmdletBinding()]
+  [CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'Low',DefaultParameterSetName = 'Connection')]
+
   param
   (
     [Parameter(Mandatory = $true, Position = 0)]
-    [string]
-    $FirstIpAddress = '192.168.1.41',
+    [ValidateScript({
+          $ptrn = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+          If($_ -match $ptrn)
+          {
+            $true
+          }
+          Else
+          {
+            Throw 'v4 IPAddress required. (ex: 192.168.10.123)'
+          }
+    })][String]$FirstIpAddress = '192.168.1.41',
     [Parameter(Mandatory = $true, Position = 1)]
-    [string]
-    $LastIpAddress = '192.168.1.50',
+    [ValidateScript({
+          $ptrn = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+          If($_ -match $ptrn)
+          {
+            $true
+          }
+          Else
+          {
+            Throw 'v4 IPAddress required. (ex: 192.168.10.123)'
+          }
+    })][String]$LastIpAddress = '192.168.1.50',
     [Parameter(Mandatory = $false, Position = 2)]
     [ValidateSet('A','B','C')]
-    [String]$Class
+    [String]$Class,
+    [Parameter(ParameterSetName = 'NetConnection')]
+    [Switch]$useNetconnetion ,
+    [Parameter(ParameterSetName = 'Workflow')]
+    [Switch]$useWorkflow
   )
-  [hashtable]$IpRange = @{}
-  function Test-Ipaddress
+  BEGIN
   {
-    param
-    (
-      [Parameter(Mandatory = $true, Position = 0)]
-    [Object]$IpAddress)
-    if(([ipaddress]$IpAddress).AddressFamily -eq 'InterNetwork')
+    [hashtable]$IpRange = @{} # For storing the address numbers that will be needed for the "ping"
+    $FullIpList = @() # For storing the full list of IP addresses.  This was put in after the other for use with the workflow
+    function Test-Ipaddress # 
     {
-      return 'IPv4'
-    }
-    elseif(([ipaddress]$IpAddress).AddressFamily -eq 'InterNetworkV6')
-    {
-      return 'Ipv6'
-    }
-  }
-  function Resolve-IpRange
-  {
-    param([Parameter(Mandatory = $true)]
-      [Object]$FirstIpAddress, 
-      [Parameter(Mandatory = $true)]
-    [Object]$LastIpAddress)
-
-    $StartIp = [int]([ipaddress]$FirstIpAddress).GetAddressBytes()[3] 
-    $EndIp = [int]([ipaddress]$LastIpAddress).GetAddressBytes()[3] 
-    if($StartIp -lt $EndIp)
-    {
-      $First3Oct = $FirstIpAddress.TrimEnd([string]$StartIp)
-    }
-    else
-    {
-      $First3Oct = $LastIpAddress.TrimEnd([string]$EndIp)
-      $StartIp = [int]([ipaddress]$LastIpAddress).GetAddressBytes()[3] 
-      $EndIp = [int]([ipaddress]$FirstIpAddress).GetAddressBytes()[3]
-    }
-    [hashtable]$IpRange = @{
-      First3Oct = $First3Oct
-      StartIp   = $StartIp
-      EndIp     = $EndIp
-    }
-    return $IpRange
-  }
-  
-  if( ((Test-Ipaddress -IpAddress $FirstIpAddress) -eq 'ipv4') -and ((Test-Ipaddress -IpAddress $LastIpAddress) -eq 'ipv4'))
-  {
-    $IpRange = Resolve-IpRange -FirstIpAddress $FirstIpAddress -LastIpAddress $LastIpAddress
-    for ($i = $IpRange.StartIp;$i -le $IpRange.EndIp;$i++)
-    {
-      $FullIpAdd = '{0}{1}' -f $IpRange.First3Oct, $i
-      Write-Verbose -Message ('Full Address: {0}' -f $FullIpAdd)
-      $ping = Test-NetConnection -ComputerName $FullIpAdd -InformationLevel Detailed -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-      if($ping.PingSucceeded -eq 'True')
+      param
+      (
+        [Parameter(Mandatory = $true, Position = 0)]
+      [Object]$IpAddress)
+      if(([ipaddress]$IpAddress).AddressFamily -eq 'InterNetwork')
       {
-        Write-Output -InputObject ('Ping Completed: {0}' -f $ping.ComputerName)
+        return 'IPv4'
+      }
+      elseif(([ipaddress]$IpAddress).AddressFamily -eq 'InterNetworkV6')
+      {
+        return 'Ipv6'
+      }
+    }
+  
+    function Resolve-IpRange
+    {
+      param([Parameter(Mandatory = $true)]
+        [Object]$FirstIpAddress, 
+        [Parameter(Mandatory = $true)]
+      [Object]$LastIpAddress)
+
+      $StartIp = [int]([ipaddress]$FirstIpAddress).GetAddressBytes()[3] 
+      $EndIp = [int]([ipaddress]$LastIpAddress).GetAddressBytes()[3] 
+      if($StartIp -lt $EndIp)
+      {
+        $First3Oct = $FirstIpAddress.TrimEnd([string]$StartIp)
       }
       else
       {
-        Write-Verbose -Message ('Ping Failed: {0}' -f $FullIpAdd)
+        $First3Oct = $LastIpAddress.TrimEnd([string]$EndIp)
+        $StartIp = [int]([ipaddress]$LastIpAddress).GetAddressBytes()[3] 
+        $EndIp = [int]([ipaddress]$FirstIpAddress).GetAddressBytes()[3]
+      }
+      [hashtable]$IpRange = @{
+        First3Oct = $First3Oct
+        StartIp   = $StartIp
+        EndIp     = $EndIp
+      }
+      return $IpRange
+    }
+
+    workflow Test-WFConnection 
+    {
+      param(
+
+        [string[]]$Computers
+
+      )
+
+      foreach -parallel ($computer in $Computers) 
+      {
+        Test-Connection -ComputerName $computer -Count 1 -ErrorAction SilentlyContinue
       }
     }
   }
-  else
+  PROCESS
   {
-    Write-Error -Message 'Not v4 Address'
+    if( ((Test-Ipaddress -IpAddress $FirstIpAddress) -eq 'ipv4') -and ((Test-Ipaddress -IpAddress $LastIpAddress) -eq 'ipv4'))
+    {
+      $IpRange = Resolve-IpRange -FirstIpAddress $FirstIpAddress -LastIpAddress $LastIpAddress
+      for ($i = $IpRange.StartIp;$i -le $IpRange.EndIp;$i++)
+      {
+        $FullIpList += '{0}{1}' -f $IpRange.First3Oct, $i # Builds the list of IP Addresses.  This was put in after the fact for use with the workflow.
+      }
+        
+      if($useWorkflow)
+      {
+        Test-WFConnection -Computers $FullIpList
+      }
+      else
+      {
+        foreach($FullIpAdd in $FullIpList)
+        {
+          Write-Verbose -Message ('Full Address: {0}' -f $FullIpAdd)
+          if($useNetconnetion)
+          {
+            $ping = Test-NetConnection -ComputerName $FullIpAdd -InformationLevel Detailed -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+            $PingResult = $ping.PingSucceeded
+          }
+          else 
+          {
+            $ping = Test-Connection -ComputerName $FullIpAdd -Count 1 -Quiet
+            $PingResult = $ping
+          }
+          if($PingResult -eq 'True')
+          {
+            Write-Output -InputObject ('Ping Completed: {0}' -f $FullIpAdd)
+          }
+          else
+          {
+            Write-Verbose -Message ('Ping Failed: {0}' -f $FullIpAdd)
+          }
+        }
+      }
+    }
+    else
+    {
+      Write-Error -Message 'Not v4 Address'
+    }
   }
+  END
+  {}
 }
+
+
+
+# This can be deleted, it only is used for testing.  
+# Finds the local gateway and machine IP and pings that range.
+# What it really shows is how long it takes.
+$Workstation = $env:COMPUTERNAME
+$NicServiceName = (Get-WmiObject -Class win32_networkadapter -Filter 'netconnectionstatus = 2' | Where-Object -FilterScript {
+    $_.Description -notmatch 'virtual'
+}).ServiceName
+$NIC = Get-WmiObject -Class Win32_NetworkAdapterConfiguration |
+Where-Object -Property ServiceName -EQ -Value $NicServiceName |
+Select-Object -Property *  
+
+Ping-IpRange -FirstIpAddress $($NIC.DefaultIPGateway[0]) -LastIpAddress $($NIC.IPAddress[0]) -Verbose -useWorkflow
 
